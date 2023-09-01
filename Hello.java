f0ba1205134eab2a5fb7fe9f29124a7f5db29e46
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-09-01 17:11:13 -0400

    added variables

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("How is everyone doing?");
+        System.out.println("A");
+        System.out.println("B");
     }
 }
